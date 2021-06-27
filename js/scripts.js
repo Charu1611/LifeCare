3a230caa0b38920c8abfe7ba36a87e4aae6c1828
Charu1611 <bansalc981@gmail.com>
2021-06-27 13:41:12 +0530

    update news section

--- js/scripts.js
+++ js/scripts.js
@@ -45,3 +45,21 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
+
+
+
+$(document).ready(function() {
+  $("#news-slider").owlCarousel({
+      items : 3,
+      itemsDesktop:[1199,3],
+      itemsDesktopSmall:[980,2],
+      itemsMobile : [600,1],
+      navigation:true,
+      navigationText:["",""],
+      pagination:true,
+      loop: true,
+      autoplay:true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true
+  });
+});
